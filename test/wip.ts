b2cac6989de3b4ceef18ac4e34d2637da3cdcd74
mechanik-daniel <daniel@outburn.co.il>
2025-05-25 12:06:42 +0300

    test: cleanup for test:wip

--- test/wip.ts
+++ test/wip.ts
@@ -1,11 +1,19 @@
+/**
+ * This script is for manual testing of specific snapshot generation scenarios.
+ * Use this during development - just replace the profileId with the one you want to test.
+ * To run this script, execute `npm run test:wip`
+ */
+
 import { FhirSnapshotGenerator } from 'fhir-snapshot-generator';
 import fs from 'fs-extra';
 import path from 'path';
 
 const cachePath = './test/.test-cache';
 
+// Replace with the actual profile ID you want to test
+const profileId: string = 'SimpleMonopolyExtensionVariation1';
+
 const applyDiffTest = async () => {
-  const profileId: string = 'language';
   const fsg = await FhirSnapshotGenerator.create({ cachePath, context: ['fsg.test.pkg#0.1.0', 'il.core.fhir.r4#0.17.0'] });
   const snapshot = await fsg.getSnapshot(profileId);
   const original = await fsg.getFpe().resolve({ url: snapshot.url, resourceType: 'StructureDefinition' });
