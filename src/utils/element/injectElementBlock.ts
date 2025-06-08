fa8f7ffea26c3f648cd0b1c2a927bb7197302356
mechanik-daniel <daniel@outburn.co.il>
2025-06-08 15:31:08 +0300

    fix: comment typo

--- src/utils/element/injectElementBlock.ts
+++ src/utils/element/injectElementBlock.ts
@@ -8,7 +8,7 @@ import { ElementDefinition } from '../../../types';
 /**
  * Injects a block of elements into the working snapshot array at the specified injection point.
  * The injection point is the id of the element where the block will be injected.
- * The block will replace the existing element at the injection point, and all of it's children (slices, elements).
+ * The block will replace the existing element at the injection point, and all of its children (slices, elements).
  * @param elements working snapshot array
  * @param injectionPoint the id of the element the block will replace
  * @param elementBlock the block of elements to inject
