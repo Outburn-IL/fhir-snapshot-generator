9c4b273dd471c2a570046234babd6b3f41a6e15e
mechanik-daniel <daniel@outburn.co.il>
2026-02-01 12:39:11 +0200

    feat: implement single-flight mechanism for concurrent snapshot generation

--- src/utils/misc/index.ts
+++ src/utils/misc/index.ts
@@ -3,3 +3,12 @@ export { resolveBasePackage } from './resolveBasePackage';
 export { DefinitionFetcher } from './definitionFetcher';
 export { initCap } from './initCap';
 export { versionedCacheDir } from './getVersionedCacheDir';
+export {
+  singleFlight,
+  getInflightKey,
+  getLockFilePath,
+  hasInflightOperation,
+  getInflightCount,
+  clearInflightOperations
+} from './singleflight';
+export type { SingleFlightResult, SingleFlightOptions } from './singleflight';
